084e0b3ab4b30d923570723a00bd1b8874c21d2f
SaranshPrasad <131943503+SaranshPrasad@users.noreply.github.com>
2023-12-17 13:46:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ choices.forEach((ch)  => {
 });
 
 function playSound(name) {
-  var audio = new Audio( "/sounds/" + name + ".mp3");
+  var audio = new Audio( name + ".mp3");
   audio.play();
 }
 function drawGame() {
